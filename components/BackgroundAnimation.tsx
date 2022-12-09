c4a38b38c483b3c00222942a8a6bfc9dbaf940f0
Himat Singh <singhimat99@gmail.com>
2022-12-08 20:13:33 -0800

    minor background change

--- components/BackgroundAnimation.tsx
+++ components/BackgroundAnimation.tsx
@@ -5,9 +5,9 @@ type Props = {};
 export default function BackgroundAnimation({}: Props) {
     return (
         <>
-            <div className="w-full h-screen absolute bg-hero-background bg-cover bg-center opacity-5">
+            {/* <div className="w-full h-screen absolute bg-hero-background bg-cover bg-center opacity-5">
                 <div className="w-full h-full backdrop-grayscale"></div>
-            </div>
+            </div> */}
             <div className="relative h-full -z-30">
                 <div className="bg"></div>
                 <div className="bg bg2"></div>
