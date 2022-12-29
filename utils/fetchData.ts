7b03d93114e27ab55ef13935074f9cc75bd20126
Himat Singh <singhimat99@gmail.com>
2022-12-29 15:06:20 -0800

    hard coded url for pageInfo

--- utils/fetchData.ts
+++ utils/fetchData.ts
@@ -19,9 +19,7 @@ export const fetchSkills = async () => {
 };
 
 export const fetchPageInfo = async () => {
-    const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL!}/api/getPageInfo`
-    );
+    const response = await fetch(`${"http://localhost:59563"}/api/getPageInfo`);
     const data = await response.json();
     const pageInfo: PageInfo = data.pageInfo;
     // console.log(pageInfo);
