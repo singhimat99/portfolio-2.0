777fbd75343f27172c6995849f051ab53b03943b
Himat Singh <singhimat99@gmail.com>
2022-12-29 13:02:51 -0800

    updated sanity cli config

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '9c367sc9',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
