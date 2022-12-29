64c6be75247dacadf5e6e8222ea4b2caea6b077a
Himat Singh <singhimat99@gmail.com>
2022-12-29 13:11:54 -0800

    updated next config to deploy dangerously

--- next.config.js
+++ next.config.js
@@ -7,6 +7,18 @@ const nextConfig = {
     images: {
         domains: ["cdn.sanity.io"],
     },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
 };
 
 module.exports = nextConfig;
